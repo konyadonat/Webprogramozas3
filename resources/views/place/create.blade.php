8c45461976bfbdec93fb219d4fe838f9a2a05616
konyadonat <konya.donat@gmail.com>
2022-05-07 18:58:14 +0200

    basic place view created

--- resources/views/place/create.blade.php
+++ resources/views/place/create.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.main')
 
 @section('content')
-    TODO PLACES FORM
+<div class="card">
+    <div class="card-body">
+        <form action="{{ route('place.create') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="d-flex align-items-center mb-3">
+                <h3 class="display-3">{{ __('Add new place') }}</h3>
+                <button class="ms-auto btn btn-primary">Add new place</button>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-8 col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="mb-3">
+                                <label for="name">{{ __('Name') }}</label>
+                                <textarea class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name">{{ old('name') }}</textarea>
+                                @if ($errors->has('name'))
+                                    <p class="invalid-feedback">{{ $errors->first('name') }}</p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form> 
+    </div>
+</div>
+         
 @endsection
