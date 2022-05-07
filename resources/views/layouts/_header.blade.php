a109c8c8507a185aecd5770f69e48397f7a91cbe
konyadonat <konya.donat@gmail.com>
2022-05-07 23:23:10 +0200

    navbar finished

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,8 +4,8 @@
         <li class="nav-item"><a href="{{ route('home') }}" class="nav-link" aria-current="page">Home</a></li>
         <li class="nav-item"><a href="{{  route('grape.create')}}" class="nav-link">Grapes</a></li>
         <li class="nav-item"><a href="{{ route('place.create')}}" class="nav-link">Places</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Wine types</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Wines</a></li>
+        <li class="nav-item"><a href="{{ route('winetype.create')}}" class="nav-link">Wine types</a></li>
+        <li class="nav-item"><a href="{{ route('wine.create')}}" class="nav-link">Wines</a></li>
       </ul>
     </header>
   </div>
