3caa6534a8148eefcd87873369607ad3483fc9fa
konyadonat <konya.donat@gmail.com>
2022-05-07 23:23:02 +0200

    basic create win and winetype created

---
+++ resources/views/wine/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+    <h3>TODO Wine Form</h3>
+@endsection
---
+++ resources/views/winetype/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+    <h3>TODO WineType Form</h3>
+@endsection
