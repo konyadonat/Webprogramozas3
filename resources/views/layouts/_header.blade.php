501c0cc789cfb3755a07970ecf7ba636c6b9f224
konyadonat <konya.donat@gmail.com>
2022-05-07 18:20:17 +0200

    Revert "navbar finished"
    This reverts commit 9392b99cdb823385088a2185ada198a4711c691e.

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,8 +4,8 @@
         <li class="nav-item"><a href="{{ route('home') }}" class="nav-link" aria-current="page">Home</a></li>
         <li class="nav-item"><a href="{{  route('grape.create')}}" class="nav-link">Grapes</a></li>
         <li class="nav-item"><a href="{{ route('place.create')}}" class="nav-link">Places</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Wine types</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Wines</a></li>
       </ul>
     </header>
   </div>
