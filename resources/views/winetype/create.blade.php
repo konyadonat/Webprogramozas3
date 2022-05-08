1f58c6cdb649581b6a00d3d8e7325384db144a7e
konyadonat <konya.donat@gmail.com>
2022-05-08 21:56:01 +0200

    winetype create view finished

--- resources/views/winetype/create.blade.php
+++ resources/views/winetype/create.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.main')
 
 @section('content')
-    <h3>TODO WineType Form</h3>
+<div class="card">
+    <div class="card-body">
+        <form action="{{ route('winetype.create') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="d-flex align-items-center mb-3">
+                <h3 class="display-3">{{ __('Add new wine type') }}</h3>
+                <button class="ms-auto btn btn-primary">{{__('Add new wine type')}}</button>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-8 col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="mb-3">
+                                <label for="winetype">{{ __('Wine type') }}</label>
+                                <input type="text" class="form-control {{ $errors->has('winetype') ? ' is-invalid' : '' }}" id="winetype" placeholder="Example wine type" name="winetype" value="{{old('winetype')}}">
+                                @if ($errors->has('winetype'))
+                                    <p class="invalid-feedback">{{ $errors->first('winetype') }}</p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form> 
+    </div>
+</div>
+         
 @endsection
