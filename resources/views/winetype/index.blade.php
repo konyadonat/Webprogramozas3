b06fe4779acf20c882820b77de3be1ea23e6b937
konyadonat <konya.donat@gmail.com>
2022-05-09 22:38:29 +0200

    WineType index view created

--- resources/views/winetype/index.blade.php
+++ resources/views/winetype/index.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.main')
 
 @section('content')
-    
+<div class="container-fluid">
+    <br/>
+    <h1 class="text-center">{{__('Winetypes in the database')}}</h1>
+    <br/>
+    <h1 class="text-left"> <a href="{{route('winetype.create')}}">{{__('Add more winetypes')}}</h1></a>
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">{{__('Type')}}</th>
+        <th scope="col">{{__('Actions')}} </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <?php foreach($winetypes as $winetype): ?>
+        <td scope="row"><?=$winetype['type']?>
+    </td>
+    <td>
+        <a href="{{ route('winetype.details', ['winetype'=> $winetype])}}">{{__('Details')}}</a>
+        <a href="{{ route('winetype.edit', ['winetype'=> $winetype])}}">{{__('Update')}}</a>
+    </td>
+      </tr>
+      <?php endforeach;?>
+    </tbody>
+  </table>
+</div>
 @endsection
