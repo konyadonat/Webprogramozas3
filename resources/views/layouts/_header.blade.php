ea83274846d92cf67d0cd6f6fc3bad7d3e1f2ca8
konyadonat <konya.donat@gmail.com>
2022-05-09 13:50:57 +0200

    header modified to be able to authenticate

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -7,5 +7,36 @@
         <li class="nav-item"><a href="{{ route('winetype.create')}}" class="nav-link">Wine types</a></li>
         <li class="nav-item"><a href="{{ route('wine.create')}}" class="nav-link">Wines</a></li>
       </ul>
+
+      @auth
+      <div class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {{ Auth::user()->name }}
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li>
+                <a class="dropdown-item" href="#">
+                    {{ __('Profile') }}
+                </a>
+            </li>
+            <li>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button class="dropdown-item" type="submit">
+                        {{ __('Sign out') }}
+                    </button>
+                </form>
+            </li>
+        </ul>
+      </div>
+      @else
+      <a class="btn btn-sm btn-outline-secondary" href="{{ route('login') }}">
+        {{ __('Sign in') }}
+      </a>
+      <a class="btn btn-sm btn-success ms-3" href="{{ route('register') }}">
+        {{ __('Sign up') }}
+      </a>
+      @endauth
+      
     </header>
   </div>
