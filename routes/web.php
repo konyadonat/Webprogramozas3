7dfd3879c17d088451bbca3605891b25aad3d597
konyadonat <konya.donat@gmail.com>
2022-05-09 13:50:37 +0200

    middleware added to routes to check auth

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,30 @@ use App\Http\Controllers;
 
 Route::get('/', [Controllers\HomeController::class,'index']) -> name('home');
 
-Route::get('/addNewPlace', [Controllers\PlaceController::class,'create'])->name('place.create');
-Route::post('/addNewPlace', [Controllers\PlaceController::class,'store']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/addNewPlace', [Controllers\PlaceController::class,'create'])->name('place.create');
+    Route::post('/addNewPlace', [Controllers\PlaceController::class,'store']);
+
+    Route::get('/addNewGrape', [Controllers\GrapeController::class,'create'])->name('grape.create');
+    Route::post('/addNewGrape', [Controllers\GrapeController::class,'store']);
+
+    Route::get('/addNewWineType', [Controllers\WineTypeController::class,'create'])->name('winetype.create');
+    Route::post('/addNewWineType', [Controllers\WineTypeController::class,'store']);
+
+    Route::get('/addNewWine', [Controllers\WineController::class,'create'])->name('wine.create');
+    Route::post('/addNewWine', [Controllers\WineController::class,'store']);
+});
+
 Route::get('/place/{place}', [Controllers\PlaceController::class,'show'])->name('place.details');
 
-Route::get('/addNewGrape', [Controllers\GrapeController::class,'create'])->name('grape.create');
-Route::post('/addNewGrape', [Controllers\GrapeController::class,'store']);
+
 Route::get('/grape/{grape}', [Controllers\GrapeController::class,'show'])->name('grape.details');
 
-Route::get('/addNewWineType', [Controllers\WineTypeController::class,'create'])->name('winetype.create');
-Route::post('/addNewWineType', [Controllers\WineTypeController::class,'store']);
+
 Route::get('/winetype/{winetype}', [Controllers\WineTypeController::class,'show'])->name('winetype.details');
 
-Route::get('/addNewWine', [Controllers\WineController::class,'create'])->name('wine.create');
-Route::post('/addNewWine', [Controllers\WineController::class,'store']);
+
 Route::get('/wine/{wine}', [Controllers\WineController::class,'show'])->name('wine.details');
 
 require __DIR__.'/auth.php';
