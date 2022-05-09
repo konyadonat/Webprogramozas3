692935c35945fa03af8b78227dfa31074182ecea
konyadonat <konya.donat@gmail.com>
2022-05-09 21:57:02 +0200

    Index place view finished

--- resources/views/place/index.blade.php
+++ resources/views/place/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.main')
 
 @section('content')
-
+<div class="container-fluid">
+    <br/>
+    <h1 class="text-center">{{__('Places in the database')}}</h1>
+    <br/>
+    <h1 class="text-left"> <a href="{{route('place.create')}}">{{__('Add more places')}}</h1></a>
 <table class="table">
     <thead>
       <tr>
@@ -15,10 +19,12 @@
         <td scope="row"><?=$place['name']?>
     </td>
     <td>
-        <a href="{{ route('place.details', ['place'=> $place])}}">Megtekint</a>
+        <a href="{{ route('place.details', ['place'=> $place])}}">{{__('Details')}}</a>
+        <a href="{{ route('place.edit', ['place'=> $place])}}">{{__('Update')}}</a>
     </td>
       </tr>
       <?php endforeach;?>
     </tbody>
   </table>
+</div>
 @endsection
