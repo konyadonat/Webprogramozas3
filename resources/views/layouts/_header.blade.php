c90debfbd58d2e010dea2c00897179b72204f15e
konyadonat <konya.donat@gmail.com>
2022-05-09 21:50:17 +0200

    Footer, header adjusted

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,7 +3,7 @@
       <ul class="nav nav-pills">
         <li class="nav-item"><a href="{{ route('home') }}" class="nav-link" aria-current="page">Home</a></li>
         <li class="nav-item"><a href="{{  route('grape.create')}}" class="nav-link">Grapes</a></li>
-        <li class="nav-item"><a href="{{ route('place.create')}}" class="nav-link">Places</a></li>
+        <li class="nav-item"><a href="{{ route('place.index')}}" class="nav-link">Places</a></li>
         <li class="nav-item"><a href="{{ route('winetype.create')}}" class="nav-link">Wine types</a></li>
         <li class="nav-item"><a href="{{ route('wine.create')}}" class="nav-link">Wines</a></li>
       </ul>
