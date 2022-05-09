56745370d74833084476c2e3ce72282946185196
konyadonat <konya.donat@gmail.com>
2022-05-10 00:27:57 +0200

    Wine index view finished

---
+++ resources/views/wine/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container-fluid">
+    <br/>
+    <h1 class="text-center">{{__('Wines in the database')}}</h1>
+    <br/>
+    <h1 class="text-left"> <a href="{{route('wine.create')}}">{{__('Add more wines')}}</h1></a>
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">{{__('Namee')}}</th>
+        <th scope="col">{{__('Grape type')}}</th>
+        <th scope="col">{{__('Winemaker name')}} </th>
+        <th scope="col">{{__('Vintage')}} </th>
+        <th scope="col">{{__('Wine type id')}} </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <?php foreach($wine as $wine): ?>
+        <td scope="row"><?=$wine['name']?></td>
+        <td scope="row"><?=$wine->grape->type?></td>
+        <td scope="row"><?=$wine->user->name?></td>
+        <td scope="row"><?=$wine->vintage?></td>
+        <td scope="row"><?=$wine->winetypes_id?></td>
+    <td>
+        <a href="{{ route('wine.details', ['wine'=> $wine])}}">{{__('Details')}}</a>
+        <a href="{{ route('wine.edit', ['wine'=> $wine])}}">{{__('Update')}}</a>
+    </td>
+      </tr>
+      <?php endforeach;?>
+    </tbody>
+  </table>
+</div>
+@endsection
