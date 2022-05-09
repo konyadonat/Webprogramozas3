706d8f144c2d7be1b70a5d861db39fd47138c35d
konyadonat <konya.donat@gmail.com>
2022-05-09 21:57:18 +0200

    Create place view modified

--- resources/views/place/create.blade.php
+++ resources/views/place/create.blade.php
@@ -1,32 +1,26 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="card">
-    <div class="card-body">
-        <form action="{{ route('place.create') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="d-flex align-items-center mb-3">
-                <h3 class="display-3">{{ __('Add new place') }}</h3>
-                <button class="ms-auto btn btn-primary">{{__('Add new place')}}</button>
-            </div>
-
-            <div class="row">
-                <div class="col-lg-8 col-md-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="mb-3">
-                                <label for="name">{{ __('Name') }}</label>
-                                <input type="text" class="form-control {{ $errors->has('name') ? ' is-invalid' : '' }}" id="name" placeholder="Example name" name="name" value="{{old('name')}}">
-                                @if ($errors->has('name'))
-                                    <p class="invalid-feedback">{{ $errors->first('name') }}</p>
-                                @endif
-                            </div>
-                        </div>
+<div class=row>
+    <div class="col md-8 col-lg-6 mx-auto">
+        <div class="card">
+            <div class="card-body">
+                <h3 class="display-3 mb-3">{{__('Add places')}}</h3>
+                <form action="{{route('place.create')}}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <input type="text" class="form-control {{ $errors->has('name') ? ' is-invalid' : '' }}" id="name" placeholder="Name of the place" name="name" value="{{old('name')}}">
+                        @if ($errors->has('name'))
+                            <p class="invalid-feedback">{{ $errors->first('name') }}</p>
+                        @endif
                     </div>
-                </div>
+                    <div class="d-grid">
+                        <button class="ms-auto btn btn-primary">{{__('Add new place')}}</button>
+                    </div>
+                </form>
             </div>
-        </form> 
     </div>
+</div>
 </div>
          
 @endsection
