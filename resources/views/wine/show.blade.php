d57f09009fadc0a5668f4923fd743b25434de5ea
konyadonat <konya.donat@gmail.com>
2022-05-09 17:00:24 +0200

    basic wine show view created

--- resources/views/wine/show.blade.php
+++ resources/views/wine/show.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.main')
-
 @section('content')
-    <h1>{{ TODO}}</h1>
+    <h1>{{ $wine->name}}</h1>
+    <h1>{{ $wine->grape->type}}</h1>
+    <h1>{{ $wine->grape->place->name}}</h1>
+    <h1>{{ $wine->user->name}}</h1>
+    <h1>{{ $wine->winetypes_id}}</h1>
 @endsection
