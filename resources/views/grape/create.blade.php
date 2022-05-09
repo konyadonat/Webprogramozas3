d2a097de2bf5dc21da0189f0943b68bd513cf924
konyadonat <konya.donat@gmail.com>
2022-05-09 23:31:18 +0200

    Grape create view modified

--- resources/views/grape/create.blade.php
+++ resources/views/grape/create.blade.php
@@ -1,52 +1,39 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="card">
-        <div class="card-body">
-            <form action="{{ route('grape.create') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="d-flex align-items-center mb-3">
-                    <h3 class="display-3">{{ __('Add new grape') }}</h3>
-                    <button class="ms-auto btn btn-primary">{{__('Add new grape')}}</button>
-                </div>
-
-                <div class="row">
-                    <div class="col-lg-8 col-md-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="mb-3">
-                                    <label for="type">{{ __('Type') }}</label>
-                                <input type="text" class="form-control {{ $errors->has('type') ? ' is-invalid' : '' }}" id="type" 
-                                    placeholder="Example type" name="type" value="{{old('type')}}">
-                                @if ($errors->has('type'))
-                                    <p class="invalid-feedback">{{ $errors->first('type') }}</p>
-                                @endif
-                                </div>
-                            </div>
-                        </div>
+<div class=row>
+    <div class="col md-8 col-lg-6 mx-auto">
+        <div class="card">
+            <div class="card-body">
+                <h3 class="display-3 mb-3">{{__('Add grapes')}}</h3>
+                <form action="{{route('grape.create')}}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <input type="text" class="form-control {{ $errors->has('type') ? ' is-invalid' : '' }}" id="type" placeholder="Type of a grape" name="type" value="{{old('type')}}">
+                        @if ($errors->has('type'))
+                            <p class="invalid-feedback">{{ $errors->first('type') }}</p>
+                        @endif
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="mb-3">
-                                <label for="place_id">{{ __('Place') }}</label>
-                                <select class="form-control{{ $errors->has('place_id') ? ' is-invalid' : '' }}" name="place_id">
-                                    <option value="">{{ __('Please choose') }}</option>
-                                    @foreach($places as $place)
-                                        <option value="{{ $place->id }}" {{ old('place_id') == $place->id ? 'selected' : '' }}>
-                                            {{ $place->name }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @if ($errors->has('place_id'))
-                                    <p class="invalid-feedback">{{ $errors->first('place_id') }}</p>
-                                @endif
-                            </div>
-                        </div>
+                    <div class="mb-3">
+                        <select class="form-control{{ $errors->has('place_id') ? ' is-invalid' : '' }}" name="place_id">
+                            <option value="">{{ __('Please choose an origin') }}</option>
+                            @foreach($places as $place)
+                                <option value="{{ $place->id }}" {{ old('place_id') == $place->id ? 'selected' : '' }}>
+                                    {{ $place->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                        @if ($errors->has('place_id'))
+                            <p class="invalid-feedback">{{ $errors->first('place_id') }}</p>
+                        @endif
                     </div>
-                </div>
-        </div>
+                    <div class="d-grid">
+                        <button class="ms-auto btn btn-primary">{{__('Add new type')}}</button>
+                    </div>
+                </form>
+            </div>
     </div>
-</form>          
+</div>
+</div>
+         
 @endsection
